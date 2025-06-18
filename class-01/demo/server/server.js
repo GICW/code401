5eb67064638b838918ae36066a6c6af9664ac6da
Jason <jtimm@gicw.org>
2025-06-18 08:39:11 -0700

    added server.js and handlers

--- class-01/demo/server/server.js
+++ class-01/demo/server/server.js
@@ -8,32 +8,32 @@ const errorHandler = require('./handlers/500.js');
 const stamper = require('./middleware/stamper.js');
 
 app.get('/', stamper, (req, res) => {
-  res.status(200).send('Hello World')
-})
+  res.status(200).send('Hello World');
+});
 
 app.get('/data', stamper, (req, res) => {
   let outputObject = {
     10: "even",
     5: "odd",
-    "time": req.timestamp // we got this from the middleware
-  }
+    "time": req.timestamp
+  };
 
   res.status(200).json(outputObject);
 });
 
 app.get('/bad', (req, res, next) => {
-  next('you messsed up')
+  next('you messsed up');
 });
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
-
 function start(port) {
-  app.listen(port, () => console.log(`Server up on port ${port}`))
+  app.listen(port, () => console.log(`Server up on port ${port}`));
 }
 
 module.exports = {
   app: app,
   start: start
-}
+};
+
