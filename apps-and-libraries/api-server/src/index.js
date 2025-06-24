1338207dcbe604517acbd0f576faf4d4e58958e7
Qilin Xie <qxie@gicw.org>
2025-06-24 08:51:35 -0700

    changes

--- apps-and-libraries/api-server/src/index.js
+++ apps-and-libraries/api-server/src/index.js
@@ -4,10 +4,12 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const mongooseOptions = {
-  useNewUrlParser:true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
-mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB Connection Error:', err));
 
 require('./src/app.js').start(process.env.PORT);
