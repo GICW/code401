cc5a9b9bc1d9b6b71e4c46dc6bd3e5bc92c79fbf
Qilin Xie <qxie@gicw.org>
2025-07-08 08:18:45 -0700

    class 6 solution updates

--- class-06/solution/lab/src/auth/models/index.js
+++ class-06/solution/lab/src/auth/models/index.js
@@ -6,25 +6,35 @@ const UserModel = require('./users-model.js');
 
 const environment = process.env.NODE_ENV;
 let DATABASE_URL = process.env.DATABASE_URL;
+
 let db_config = {
   logging: false,
 };
 
-switch(environment) {
+switch (environment) {
   case 'test':
-    DATABASE_URL = 'sqlite::memory'
+    DATABASE_URL = 'sqlite::memory:';
     break;
   case 'production':
     db_config.dialectOptions = {
-      ssl: true,
-      rejectUnauthorized: false,
-    }
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    };
     break;
   case 'development':
     db_config.logging = true;
     break;
   default:
     console.log('Using default DB configuration ' + DATABASE_URL, db_config);
+    db_config.dialectOptions = {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    };
+    break;
 }
 
 const sequelize = new Sequelize(DATABASE_URL, db_config);
@@ -32,4 +42,4 @@ const sequelize = new Sequelize(DATABASE_URL, db_config);
 module.exports = {
   db: sequelize,
   User: UserModel(sequelize, DataTypes),
-}
+};
