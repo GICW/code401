fa07fcc91dcd06f1a39e80e6654d7ea3a25e618d
Qilin Xie <qxie@gicw.org>
2025-09-03 08:23:40 -0700

    updates lab31

--- class-31/solution/src/Components/Footer/Footer.jsx
+++ class-31/solution/src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Text } from '@mantine/core';
 const Footer = () => {
   return (
     <footer style={{width: '80%', margin: 'auto'}}>
-      <Text align="right" mt="lg" >&copy;2022 Code Fellows</Text>
+      <Text align="right" mt="lg" >&copy;2025 ITTP</Text>
     </footer>
   )
 };
